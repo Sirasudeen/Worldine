20b0400836a9cfcb2cff7c1356f0426eb2d46f63
Siras <sirasudeenp@gmail.com>
2025-01-24 06:39:21 -0800

    Landing page completed

--- Worldine/src/App.jsx
+++ Worldine/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from './Pages/Home.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Analytics />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
